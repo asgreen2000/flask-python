cb8fe09b83181e3715a4444b603553087dd134ba
Bùi Hữu Tiến Đạt <75929269+asgreen2000@users.noreply.github.com>
2022-07-24 14:13:50 +0700

    [Modify] profile api

--- login_with_ldap/app/engine/user/base_engine_bl.py
+++ login_with_ldap/app/engine/user/base_engine_bl.py
@@ -1,9 +1,10 @@
 from pydoc import Helper
 from .models.User import User, UserUtil, collection as users_collection
 from .helper import *
+from bson import ObjectId
 
-def find_user_by_id(id) -> User:
-    data = users_collection.find_one({'_id': id})
+def find_user_by_id(id : str) -> User:
+    data = users_collection.find_one({'_id': ObjectId(id)})
     return UserUtil.cast(data) if data else None
 
 def find_user_by_username(username) -> User:
@@ -11,7 +12,8 @@ def find_user_by_username(username) -> User:
     return UserUtil.cast(data) if data else None
 
 def insert_user(user: User) -> User:
-    users_collection.insert_one(user.to_json())
+    result = users_collection.insert_one(user.to_json())
+    user.set_id(str(result.inserted_id))
     return user
 
 def insert_if_absent(user: User) -> User:
